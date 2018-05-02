70afa176b37007995f6ffd8125d0594e217bdeb2
DENIS NAUGHTON <mjolinor75@hotmail.comssh-keygen -t rsa -b 4096 -C mjolinor75@hotmail.com>
2018-05-01 20:12:33 -0400

    Added the rest of the routes and pages

--- app.rb
+++ app.rb
@@ -8,3 +8,23 @@ require "sinatra"
 	get '/old' do
 		erb :old
 	end
+
+	get '/perspective' do 
+		erb :perspective
+	end
+
+	get '/silly' do
+		erb :silly
+	end
+
+	get '/snow' do
+		erb :snow
+	end
+
+	get '/soup' do
+		erb :soup
+	end
+
+	get '/contact' do
+		erb :contact
+	end
